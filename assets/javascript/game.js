c17a5f8b933863b56ccc9812fd7679760ac12e0b
Nick <nlweber3@gmail.com>
2017-12-18 18:42:45 -0800

    html/css

---
+++ assets/javascript/game.js
@@ -0,0 +1,28 @@
+$(document).ready(function() {
+
+	var characters = {
+		'yoda' {
+			name: 'yoda',
+			health: 100,
+			attack: 20
+		},
+		'luke' {
+			name:'luke',
+			health: 150,
+			attack: 15
+		},
+		'kylo' {
+			name:'kylo',
+			health: 140,
+			attack: 18
+		},
+		'vader' {
+			name:'vader',
+			health: 160,
+			attack: 25
+		},
+	};
+
+$("yoda").on(click); 
+	console.log("works")
+};
