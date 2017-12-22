5925d124d47b63788daf337fd5032e442af0adc8
Nick <nlweber3@gmail.com>
2017-12-21 22:34:21 -0800

    adding some jquery still not working on some functions

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,28 +1,99 @@
 $(document).ready(function() {
 
-	var characters = {
-		'yoda' {
-			name: 'yoda',
-			health: 100,
-			attack: 20
-		},
-		'luke' {
-			name:'luke',
-			health: 150,
-			attack: 15
-		},
-		'kylo' {
-			name:'kylo',
-			health: 140,
-			attack: 18
-		},
-		'vader' {
-			name:'vader',
-			health: 160,
-			attack: 25
-		},
-	};
+	var characterChoice;
+	var enemyChosen;
+	var enemyChosen = false;
 
-$("yoda").on(click); 
-	console.log("works")
-};
+	var characters =  [
+		yoda = {
+			name: "#yoda",
+			healthPower: 100,
+			attackPower: 20
+		},
+		luke = {
+			name: "#luke",
+			healthPower: 150,
+			attackPower: 15
+		},
+		kylo = {
+			name: "#kylo",
+			healthPower: 140,
+			attackPower: 18
+		},
+		vader = {
+			name: "#vader",
+			healthPower: 160,
+			attackPower: 25
+		},
+	];
+	
+	// var  characterChoice = characters [];
+
+	// var attack = characterChoice["health"] - ["attack"];
+
+});
+		// function chooseCharacter {
+		// 	characterChoice = true;
+		// 	character = char;
+		// 	console.log(result);
+
+		// }
+
+		$("#yoda_div").click(function() {
+			$("h1").text("Your Character");
+			$("#vader_div").prependTo("#enemies");
+			$("#kylo_div").prependTo("#enemies");
+			$("#luke_div").prependTo("#enemies");
+			chooseCharacter(yoda);
+
+
+		});
+
+		$("#vader_div").click(function() {
+			$("h1").text("Your Character");
+			$("#yoda_div").prependTo("#enemies");
+			$("#kylo_div").prependTo("#enemies");
+			$("#luke_div").prependTo("#enemies");
+			chooseCharacter(vader);
+
+		});
+
+		$("#kylo_div").click(function() {
+			$("h1").text("Your Character");
+			$("#yoda_div").prependTo("#enemies");
+			$("#vader_div").prependTo("#enemies");
+			$("#luke_div").prependTo("#enemies");
+			chooseCharacter(kylo);
+
+		});
+
+		$("#luke_div").click(function() {
+			$("h1").text("Your Character");
+			$("#yoda_div").prependTo("#enemies");
+			$("#kylo_div").prependTo("#enemies");
+			$("#vader_div").prependTo("#enemies");
+			chooseCharacter(luke);
+
+		});
+		
+	$("#attack_button").click(function() {
+		var healthPower = this.healthPower - this.attackPower;
+
+	});
+	function chooseCharacter(char){
+		characterChoice = true;
+		character = char;
+		console.log(character);
+	}
+	function chooseEnemy(char){
+		if(enemyChosen == false){
+			opponent = char;
+			enemyChosen = true;
+		}
+	}
+
+	
+// $(document).ready(function() {
+// $("#yoda").click(function() {
+// 	$("#luke", "#vader", "#kylo").appendTo($("#enemies"));
+// });
